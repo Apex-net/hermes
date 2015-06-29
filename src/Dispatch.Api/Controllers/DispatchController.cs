922e113dac0ecd3c0d626843e116a9cffd8d4faa
Ali Servet Donmez <asd@pittle.org>
2015-06-29 15:37:16 +0200

    Add initial draft dispatch controller

---
+++ src/Dispatch.Api/Controllers/DispatchController.cs
@@ -0,0 +1,12 @@
+﻿namespace Dispatch.Api.Controllers
+{
+    using System.Web.Http;
+    using Dispatch.Api.Models;
+
+    public class DispatchController : ApiController
+    {
+        public void Post([FromBody] Message message)
+        {
+        }
+    }
+}
