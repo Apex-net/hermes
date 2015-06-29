b22d165d72fce8d59444335475c9f197c830d236
Ali Servet Donmez <asd@pittle.org>
2015-06-29 15:29:29 +0200

    Add internal default web api configuration for Apexnet

--- src/Dispatch.Api/Global.asax.cs
+++ src/Dispatch.Api/Global.asax.cs
@@ -2,12 +2,15 @@
 {
     using System.Web;
     using System.Web.Http;
+    using Common.WebApi;
 
     public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            DefaultWebApiConfig.Register("dispatch", "0.1.0", GlobalConfiguration.Configuration);
         }
     }
 }
