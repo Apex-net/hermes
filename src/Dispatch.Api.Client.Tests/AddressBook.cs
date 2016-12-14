141523446411d442af636028b5a4cfcfef2e1d96
Ali Servet Donmez <asd@pittle.org>
2016-12-14 09:55:43 +0100

    Fix addresses in the address book ;-)

--- src/Dispatch.Api.Client.Tests/AddressBook.cs
+++ src/Dispatch.Api.Client.Tests/AddressBook.cs
@@ -5,15 +5,15 @@ namespace Dispatch.Api.Client.Tests
     // ReSharper disable UnusedMember.Global
     public static class AddressBook
     {
-        public static readonly MailAddress Ali = new MailAddress("a.donmez@apexnet.it", "Ali Servet Donmez");
+        public static readonly MailAddress Ali = new MailAddress("a.donmez@wedoit.io", "Ali Servet Donmez");
 
-        public static readonly MailAddress Stefano = new MailAddress("s.teodorani@apexnet.it", "Stefano Teodorani");
+        public static readonly MailAddress Stefano = new MailAddress("s.teodorani@wedoit.io", "Stefano Teodorani");
 
-        public static readonly MailAddress Andrea = new MailAddress("a.caliesi@apexnet.it", "Andrea Calisesi");
+        public static readonly MailAddress Andrea = new MailAddress("a.calisesi@wedoit.io", "Andrea Calisesi");
 
-        public static readonly MailAddress Fabio = new MailAddress("f.vassura@apexnet.it", "Fabio Vassura");
+        public static readonly MailAddress Fabio = new MailAddress("f.vassura@wedoit.io", "Fabio Vassura");
 
-        public static readonly MailAddress AgendaSviluppo = new MailAddress("agesvil@apexnet.it", "AgendaBocconi (Sviluppo)");
+        public static readonly MailAddress AgendaSviluppo = new MailAddress("agesvil@wedoit.io", "AgendaBocconi (Sviluppo)");
     }
     // ReSharper restore UnusedMember.Global
 }
